9181bf9261e4a3f1626259a2273c806a2328bb33
Vayne-Lover <406378362@qq.com>
2017-03-10 13:45:28 +0800

    Add calc_statistics.

--- P1 stroop_test/script.py
+++ P1 stroop_test/script.py
@@ -4,47 +4,61 @@ import csv
 import math
 
 def func_read_csv(file,*args):
-    result1=[]
-    result2=[]
+    result1 = []
+    result2 = []
 
     with open(file) as csvfile:
-        reader=csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile)
         for row in reader:
             result1.append(float(row["Congruent"]))
             result2.append(float(row["Incongruent"]))
-    print(result1,result2)
-    return (result1,result2)
+    # print(result1,result2)
+    return (result1, result2)
 
-def calc_average(list_para):
-    result=0.0
-    length=len(list_para)
+def calc_mean(list_para):
+    result = 0.0
+    length = len(list_para)
     for i in list_para:
         result += i
-    return result/length
+    return result / length
 
-def calc_variance(list_para, avg_para):
-    result=0.0
+def calc_variance(list_para, mean_para):
+    result = 0.0
     for i in list_para:
-        result+=(i-avg_para)**2
-    length=len(list_para)-1
-    return result/length
+        result += (i - mean_para) ** 2
+    length = len(list_para) - 1
+    if length <= 0:
+        length = 1
+    return result / length
 
 def calc_standard_variance(var_para):
     return math.sqrt(var_para)
 
-def main():
-    result1,result2 = func_read_csv("stroopdata.csv", "Congruent", "Incongruent")
-    avg1=calc_average(result1)
-    var1=calc_variance(result1,avg1)
-    sd1=calc_standard_variance(var1)
-    print(avg1,var1,sd1)
+def calc_statistics(list_para):
+    mean1 = calc_mean(list_para)
+    var1 = calc_variance(list_para, mean1)
+    sd1 = calc_standard_variance(var1)
+    return [mean1, var1, sd1]
 
-if __name__=="__main__":
+def func_print_result(list_para):
+    print("*" * 10)
+    print("Print statistics")
+    print("Mean is :", list_para[0])
+    print("Variance is :", list_para[1])
+    print("SD is :", list_para[2])
+    print("*" * 10)
+
+def main():
+    result1, result2 = func_read_csv("stroopdata.csv", "Congruent", "Incongruent")
+    func_print_result(calc_statistics(result1))
+    func_print_result(calc_statistics(result2))
+
+if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print(e)
         print("Error")
+        print(e)
     else:
         print("Success!")
     finally:
